7d9361ac5933ae06d2b75a7e424c3d714dfb7c3c
Idrissa SOKHONA <i.sokhona84@gmail.com>
2021-02-09 23:08:55 +0100

    setting up the development environment

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const gulp = require('gulp');
+const del = require('del');
+const ts = require('gulp-typescript');
+
+let options = {
+    output: '.',
+    compile: [ './src/**/*.ts' ],
+    remove: [ 'lib/', 'test/', 'app.js' ]
+};
+
+gulp.task('clean', () => {
+    return del(options.remove);
+});
+
+gulp.task('compile', () => {
+    const tsProject = ts.createProject(path.resolve('./tsconfig.json'));
+    const tsResult = gulp.src(options.compile).pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest(path.resolve(options.output)));
+});
+
+gulp.task('build', gulp.series('clean', 'compile'));
+gulp.task('default', gulp.series('build'));
